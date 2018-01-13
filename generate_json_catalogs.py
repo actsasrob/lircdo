2a54505185313f33edb53f4f4b3610335cd6765d
actsasrob <acts.as.rob@gmail.com>
2018-01-12 19:24:11 -0500

    Refactor script to parse new format for meta: lines in LIRC scripts

--- generate_json_catalogs.py
+++ generate_json_catalogs.py
@@ -7,15 +7,14 @@ import re
 import subprocess
 
 env_file=".env"
+
 catalog_internal_file="./catalog_internal.json"
 catalog_external_file="./catalog_external.json"
 
 with open('sslcert/cacert.pem', 'r') as myfile:
     cacert=myfile.read()
-#    cacert=myfile.read().replace('\n', '')
 
 catalog_internal={'intents': []}
-#catalog_external={'intents': []}
 catalog_external={'ca': cacert}
 
 def read_env_vars(envfilename = '.env'): 
@@ -26,8 +25,9 @@ def read_env_vars(envfilename = '.env'):
          myvars[name.strip()] = var.strip('\'"\n')
    return myvars
 
+# Parse LIRC script and return intent dictionary
 def process_lircscript(filename):
-   retVal={ 'numargs': 0 }
+   retVal={ 'numargs': 0, 'default_component': False }
    p1 = subprocess.Popen(['grep', 'meta:', filename], stdout=subprocess.PIPE)
    # Run the command
    output = p1.communicate()[0]
@@ -35,14 +35,21 @@ def process_lircscript(filename):
    output = output.splitlines()
    #print "output="
    #print repr(output)
+   multivalued_keys = [ 'action', 'component' ]
    for line in output:
       #print "line: " + line
       m = re.search( r'^(.*meta: )([^=]+)=(.*)$', line, re.M|re.I)
       if m:
          thekey,thevalue=m.group(2,3)
-         retVal[thekey] = thevalue
+         thekey = thekey.lower()
+         if thekey == 'default_component':
+            retVal[thekey] = thevalue.lower in ['true', '1', 't', 'y', 'yes']
+         elif thekey in multivalued_keys:
+            retVal[thekey] = [x.strip() for x in thevalue.split(',')] 
+         else:
+            retVal[thekey] = thevalue
    missingfields=False
-   for key in ('name', 'displayname', 'intent'):
+   for key in ('name', 'displayname', 'intent', 'action'):
       if key not in retVal:
          print 'error: script={} missing {} key'.format(filename, key)
          missingfields=True
@@ -90,30 +97,30 @@ lircscripts=glob.glob(LIRCSCRIPTS_LOCATION + '/*.sh')
 lircscripts=sorted(lircscripts)
 #print "lircscripts: ", lircscripts
 for filename in lircscripts:
-   dict = process_lircscript(filename)
-   if len(dict) > 0:
-      #intents = catalog_external['intents']
-      #intents.append(dict)
-      #catalog_external['intents']=intents
-      
-      internal_dict = copy.deepcopy(dict)
+   intent_dict = process_lircscript(filename)
+   if len(intent_dict) > 0:
+      internal_dict = copy.deepcopy(intent_dict)
       internal_dict['lircscript']=filename
       intents = catalog_internal['intents']
       intents.append(internal_dict)
       catalog_internal['intents']=intents
-#print "catalog_internal"
-#print(json.dumps(catalog_internal, indent=4))
+
+print "catalog_internal"
+print(json.dumps(catalog_internal, indent=4))
 
 #print "\n\n"
 
-print "catalog_external"
-print(json.dumps(catalog_external, indent=4))
+#print "catalog_external"
+#print(json.dumps(catalog_external, indent=4))
+
+#with open('data.txt', 'wt') as out:
+#    res = json.dump(obj, out, sort_keys=True, indent=4, separators=(',', ': '))
 
 with open(catalog_internal_file, 'w') as outfile:
-    json.dump(catalog_internal, outfile)
+    json.dump(catalog_internal, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
 with open(catalog_external_file, 'w') as outfile:
-    json.dump(catalog_external, outfile)
+    json.dump(catalog_external, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
 print 'info: internal catalog written to {}'.format(catalog_internal_file)
 print 'info: external catalog written to {}'.format(catalog_external_file)
