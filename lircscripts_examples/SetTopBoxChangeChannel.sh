d829688d3c8a057674ecb6876c90f1d5d212837e
actsasrob <acts.as.rob@gmail.com>
2018-01-14 10:47:41 -0500

    Implement SetTopBox change channel LIRC script

--- lircscripts_examples/SetTopBoxChangeChannel.sh
+++ lircscripts_examples/SetTopBoxChangeChannel.sh
@@ -3,12 +3,21 @@
 # meta: name=settopboxchangechannel
 # meta: displayname=Set Top Box Change Channel
 # meta: intent=channel_action
-# meta: action=CHANGE_CHANNEL
+# meta: action=CHANNEL_CHANGE
 # meta: component=COMPONENT_STB
 # meta: default_component=true
 # meta: numargs=1
 
-irsend SEND_ONCE Motorola_QIP6200-2 KEY_7
-irsend SEND_ONCE Motorola_QIP6200-2 KEY_4
-irsend SEND_ONCE Motorola_QIP6200-2 KEY_6
+status=0
+
+if [ "$#" -eq 1 ]; then
+   argument="$1"
+   if [[ "$argument" =~ ^[0-9]+$ ]] && [ ${#argument} -lt 5 ]; then
+      for ((i=0; i<${#argument}; i++)); do
+        irsend SEND_ONCE Motorola_QIP6200-2 "KEY_${argument:i:1}"
+        status=$?
+      done
+   fi
+fi
+
 exit $?
