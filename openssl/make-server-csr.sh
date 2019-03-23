86840e3c68f70f66720556a4f40be36bc61d26a6
actsasrob <acts.as.rob@gmail.com>
2019-03-23 12:48:24 -0400

    Add SAN entry in certificate for lircdo server FQDN

--- openssl/make-server-csr.sh
+++ openssl/make-server-csr.sh
@@ -5,6 +5,7 @@ if [ -f servercert.csr ]; then
    echo "info: servercert.csr already exists. skipping creation of servercert.csr"
    sleep 3
 else
+   sed -i "s/^DNS.1.*/DNS.1 = $FQDN/" ./openssl-server.cnf
    echo -ne "\n\n\nlircdo\n$FQDN\nlircdo@example.com\n" | openssl req -config openssl-server.cnf -newkey rsa:2048 -sha256 -nodes -out servercert.csr -outform PEM
 fi
 
