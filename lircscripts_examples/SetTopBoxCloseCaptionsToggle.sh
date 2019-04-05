7a8f857a208e48e9970c8e59e869571ffa93091c
actsasrob <acts.as.rob@gmail.com>
2019-04-05 17:41:43 -0400

    Add close captions script. Set permissions

---
+++ lircscripts_examples/SetTopBoxCloseCaptionsToggle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# meta: name=settopboxclosecaptionstoggle
+# meta: displayname=Set Top Box Toggle Close Captions 
+# meta: intent=lircdo
+# meta: action=CLOSE_CAPTIONS
+# meta: component=COMPONENT_STB
+# meta: default_component=true
+# meta: numargs=0
+
+irsend SEND_ONCE Motorola_QIP6200-2 KEY_NUMERIC_STAR
+exit $?
